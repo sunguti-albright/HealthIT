8e2c74a5e90845a40e23f6752240703f31568e0e
albright <sungutialbright@gmail.com>
2022-09-27 17:55:13 +0300

    add project urls

---
+++ registry/urls.py
@@ -0,0 +1,23 @@
+from django.urls import path as url
+from . import views
+from django.urls import re_path, path , include
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import *
+from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView, LogoutView 
+from .models import *
+
+urlpatterns = [
+    path('myprofile/',my_profile_view,name ='profile'),
+    path('' , home , name='home'),
+    path('search/',search_results, name ='search_results'),
+    path('delete/<int:pk>',delete_project, name = 'deletepost'), 
+    path('update/<str:pk>',update_project, name = 'updatepost'), 
+    re_path(r'^api/profile/$', views.ProfileList.as_view()),
+    re_path(r'^api/report/$', views.ReportList.as_view()),
+]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
