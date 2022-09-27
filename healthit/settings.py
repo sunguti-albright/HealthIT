8088e2fff4125e196e25aeade2186e491a3be1a8
albright <sungutialbright@gmail.com>
2022-09-27 23:11:54 +0300

    deployment

--- healthit/settings.py
+++ healthit/settings.py
@@ -111,12 +111,12 @@ if config('MODE')=="dev":
     }
 }
 #production
-# else:
-#    DATABASES = {
-#        'default': dj_database_url.config(
-#            default=config('DATABASE_URL')
-#        )
-#    }
+else:
+   DATABASES = {
+       'default': dj_database_url.config(
+           default=config('DATABASE_URL')
+       )
+   }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
